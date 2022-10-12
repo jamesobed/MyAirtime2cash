db3e39e86f9feaa90cc9f80213594d12c7616c09
jamesobed <jamesobed4@gmail.com>
2022-10-12 17:45:31 +0100

    backend

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -1,10 +1,8 @@
 import math from "../test";
-import Login from "../components/Login/Login";
+// import Login from "../components/Login/Login";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(math(1, 2)).toBe(3);
 });
 
-test("Login component renders without crashing", () => {
-  
-});
+test("Login component renders without crashing", () => {});
