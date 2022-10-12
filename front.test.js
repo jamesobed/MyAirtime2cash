c95ee6e0223f4f5a3a21c0619a8a5b331141e023
jamesobed <jamesobed4@gmail.com>
2022-10-12 17:48:54 +0100

    tested

---
+++ front.test.js
@@ -0,0 +1,8 @@
+import math from "../test";
+// import Login from "../components/Login/Login";
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(math(1, 2)).toBe(3);
+});
+
+test("Login component renders without crashing", () => {});
